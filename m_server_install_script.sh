707ae3d80f41a5ff2aa8241bb4946a92765abe63
Miles <miles@mrqdt.org>
2023-08-20 17:30:54 +0200

    Update m_server_install_script.sh

--- m_server_install_script.sh
+++ m_server_install_script.sh
@@ -50,7 +50,7 @@ function option_2() {
     sudo ln -s /snap/bin/certbot /usr/bin/certbot
     sudo snap set certbot trust-plugin-with-root=ok
     sudo snap install certbot-dns-cloudflare
-    read -p '^[[31mFoobar^[[0m: ' -n 1 -r
+    read -p $'\e[31mFoobar\e[0m: ' -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
