6c93e63bf91bf460e42d01bac24324f82ae6e56a
Miles <miles@mrqdt.org>
2023-08-20 04:01:11 +0200

    Update m_server_install_script.sh

--- m_server_install_script.sh
+++ m_server_install_script.sh
@@ -63,7 +63,6 @@ else
 	echo "$DIR directory does not exist."
  echo -e "${red} $DIR directory does not exist! Please finish the installation of certbot!${clear}"
  echo -e "${magenta} returning to main menu
- main
 fi
    
     
