e5db2aee3df172bde5ee99a88bc93cee6b83ca20
Miles <miles@mrqdt.org>
2023-08-21 23:51:53 +0200

    Update open_menu.sh

--- open_menu.sh
+++ open_menu.sh
@@ -191,7 +191,7 @@ function option_5() {
     word=/var/www/$wordpress
     if [ -d "$word" ];
     then
-        echo -e -n "${bg_blue}Folder $word already exists! Please remove the folder! Returning to main menu${clear}"
+        echo -e "${bg_blue}Folder $word already exists! Please remove the folder! Returning to main menu${clear}"
         pause 'Press [Enter] key to continue...'
     else
         sudo apt install unzip -y
